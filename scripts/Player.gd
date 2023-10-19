d5485f3062c33664912b98edaf167ffea58debf9
GabrielCPinto <gabrielcostap112@gmail.com>
2023-10-19 19:44:48 -0300

    video 12

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -6,8 +6,9 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var is_jumping := false
 var knockback_vector := Vector2.ZERO
+var direction
+var is_hurt := false
 
 @export var player_life := 3
 
@@ -22,24 +23,19 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		is_jumping = true
-	elif is_on_floor():
-		is_jumping = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_axis("ui_left", "ui_right")
 	
 	if direction:
 		velocity.x = direction * SPEED
 		animation.scale.x = direction
-		if !is_jumping:
-			animation.play("run")
-	elif is_jumping:
-			animation.play("jump")
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animation.play("idle")
+	
+	_set_state()
 	
 	if knockback_vector != Vector2.ZERO:
 		velocity = knockback_vector
@@ -66,7 +62,23 @@ func take_damage(knockback_force := Vector2.ZERO, duration := 0.25):
 		knockback_tween.tween_property(self, "knockback_vector", Vector2.ZERO, duration)
 		animation.modulate = Color(1,0,0,1)
 		knockback_tween.tween_property(animation, "modulate", Color(1,1,1,1), duration)
-		
+	
+	is_hurt = true
+	await get_tree().create_timer(.3).timeout
+	is_hurt = false
+
+func _set_state():
+	var state = "idle"
+	
+	if is_hurt:
+		state = "hurt"
+	elif !is_on_floor():
+		state = "jump"
+	elif direction:
+		state = "run"
+	
+	if animation.name != state:
+		animation.play(state)
 
 func follow_camera(camera):
 	var camera_path = camera.get_path()
