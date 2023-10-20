42a21afc72b2af12ccc938dd85850f29d8f941ed
GabrielCPinto <gabrielcostap112@gmail.com>
2023-10-20 16:17:35 -0300

    video 14

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -88,6 +88,7 @@ func follow_camera(camera):
 func _on_head_collider_body_entered(body):
 	if body.has_method("break_sprite"):
 		body.hitpoints -= 1
+		body.create_coin()
 		if body.hitpoints <= 0:
 			body.break_sprite()
 		else:
