7e1c5deef21b2b37b590c4050ffa8840e69b1756
GabrielCPinto <gabrielcostap112@gmail.com>
2023-10-25 14:47:28 -0300

    video 23

--- scripts/world_01.gd
+++ scripts/world_01.gd
@@ -10,9 +10,6 @@ func _ready():
 	Globals.player.follow_camera(camera)
 	Globals.player.player_has_died.connect(reload_game)
 	control.time_is_up.connect(reload_game)
-	Globals.coins = 0
-	Globals.score = 0
-	Globals.player_life = 3
 
 func reload_game():
 	await get_tree().create_timer(1.0).timeout
@@ -21,6 +18,8 @@ func reload_game():
 	Globals.player = player
 	Globals.player.follow_camera(camera)
 	Globals.player.player_has_died.connect(reload_game)
+	Globals.coins = 0
+	Globals.score = 0
+	Globals.player_life = 3
 	Globals.respawn_player()
 #	get_tree().reload_current_scene()
-	
