604f923da0c3986247c21082d711e5a161368736
GabrielCPinto <gabrielcostap112@gmail.com>
2023-10-25 12:36:42 -0300

    video 22

---
+++ scripts/checkpoint.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@onready var anim = $anim
+
+var is_active = false
+
+func _on_body_entered(body):
+	if body.name != "player" or is_active:
+		activate_checkpoint()
+
+func activate_checkpoint():
+	Globals. current_checkpoint = self
+	anim.play("raising")
+	is_active = true
+
+
+func _on_anim_animation_finished():
+	if anim.animation == "raising":
+		anim.play("checked")
--- scripts/world_01.gd
+++ scripts/world_01.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
 @onready var control = $HUD/control
-
+@onready var player_scene = preload("res://actors/player.tscn")
 @onready var player := $Player as CharacterBody2D
 @onready var camera := $Camera as Camera2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player.follow_camera(camera)
-	player.player_has_died.connect(reload_game)
+	Globals.player = player
+	Globals.player.follow_camera(camera)
+	Globals.player.player_has_died.connect(reload_game)
 	control.time_is_up.connect(reload_game)
 	Globals.coins = 0
 	Globals.score = 0
@@ -15,5 +16,11 @@ func _ready():
 
 func reload_game():
 	await get_tree().create_timer(1.0).timeout
-	get_tree().reload_current_scene()
+	var player = player_scene.instantiate()
+	add_child(player)
+	Globals.player = player
+	Globals.player.follow_camera(camera)
+	Globals.player.player_has_died.connect(reload_game)
+	Globals.respawn_player()
+#	get_tree().reload_current_scene()
 	
